cadcb9c94741ca5e3beeacb3eaa5561e7fffc261
Claudio Perroni <c.perroni@ifortech.com>
2025-07-09 11:48:12 +0200

    force dynamic

--- app/api/draft-mode/enable/route.ts
+++ app/api/draft-mode/enable/route.ts
@@ -1,6 +1,8 @@
 import { client } from "@/sanity/lib/client";
 import { defineEnableDraftMode } from "next-sanity/draft-mode";
 
+export const dynamic = "force-dynamic";
+
 export const { GET } = defineEnableDraftMode({
   client: client.withConfig({
     token: process.env.SANITY_VIEWER_TOKEN,
